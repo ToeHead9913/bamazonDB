f04b494ca0ed468ae1c36cd11853a777d6c2c556
Trenton Gharst <toe9913@gmail.com>
2019-11-17 20:40:30 -0600

    push

--- favorites.sql
+++ favorites.sql
@@ -1,1 +1,21 @@
+DROP DATABASE IF EXISTS favoritesDB;
 
+CREATE DATABASE favoritesDB;
+
+USE favoritesDB;
+
+CREATE TABLE foods(
+  id INT NOT NULL AUTO_INCREMENT,
+  kind VARCHAR(45) NULL,
+  score DECIMAL(100) NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO foods (kind, score)
+VALUES ("burgers", 120);
+
+INSERT INTO foods (kind, score)
+VALUES ("pizza", 90);
+
+INSERT INTO foods (kind, score)
+VALUES ("tacos", 100);
